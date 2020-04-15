5c8489d8c54ed011ac0d76e7c51696f0acf974e2
Mahesh <54623929+maahi07m@users.noreply.github.com>
2020-04-15 21:07:23 +0530

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,16 @@
+class MyClass:
+	def method(self):
+		return 'instance method called',self
+	
+	@classmethod
+	def classmethod(cls):
+		return 'class method called', cls
+	
+	@staticmethod
+	def staticmethod():
+		return 'static method called'
+
+myclass = MyClass() 
+print(myclass.method())
+print(myclass.classmethod())
+print(myclass.staticmethod())
