cf50eb1c0732b636750543128b506a61956a45ef
Mahesh <54623929+maahi07m@users.noreply.github.com>
2020-04-21 15:13:03 +0530

    Update Assignment-5.py

--- Assignment-5.py
+++ Assignment-5.py
@@ -11,6 +11,9 @@ class Store:
 	def add_item(self,item):
 		self.store_item.append(item)
 	
+	def count(self):
+		return len(self.store_item)
+	
 	def filter(self, q_object):
 		store_obj = Store()
 		if q_object.operation == "EQ":
@@ -58,13 +61,72 @@ class Store:
 					store_obj.add_item(i)
 		
 		elif q_object.operation == "CONTAINS":
-			for i in self.store_item:
-				if i.name.__contains__(q_object.value) or i.category.__contains__(q_object.value):
-					store_obj.add_item(i)
+			if q_object.field == "name":
+				for i in self.store_item:
+					if (i.name.__contains__(q_object.value)):
+						store_obj.add_item(i)
+			else:
+				for i in self.store_item:
+					if (i.category.__contains__(q_object.value)):
+						store_obj.add_item(i)
 					
 		return store_obj
 		
-	
+	def exclude(self, q_object):
+		store_obj = Store()
+		if q_object.operation == "EQ":
+			for i in self.store_item:
+				if (q_object.value != i.name and q_object.value != i.price and q_object.value != i.category):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "GT":
+			for i in self.store_item:
+				if not(q_object.value < i.price):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "GTE":
+			for i in self.store_item:
+				if not(q_object.value <= i.price):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "LT":
+			for i in self.store_item:
+				if not(q_object.value > i.price):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "LTE":
+			for i in self.store_item:
+				if not(q_object.value >= i.price):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "STARTS_WITH":
+			for i in self.store_item:
+				if not(i.name.startswith(q_object.value)or i.category.startswith(q_object.value)):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "ENDS_WITH":
+			for i in self.store_item:
+				if not(i.name.endswith(q_object.value) or i.category.endswith(q_object.value)):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "IN":
+			for i in self.store_item:
+				if not(i.name in q_object.value or i.category in q_object.value or i.price in q_object.value):
+					store_obj.add_item(i)
+		
+		elif q_object.operation == "CONTAINS":
+			if q_object.field == "name":
+				for i in self.store_item:
+					if not(i.name.__contains__(q_object.value)):
+						store_obj.add_item(i)
+			else:
+				for i in self.store_item:
+					if not(i.category.__contains__(q_object.value)):
+						store_obj.add_item(i)
+					
+		return store_obj
+		
+		
 class Item:
 	def __init__(self, name=None, price=0, category=None):
 		self.name = name
